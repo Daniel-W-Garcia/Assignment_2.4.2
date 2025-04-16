838a5ca56dc2f984bb40ab6381f3e807221648ab
itodobien <daniel.w.garcia@outlook.com>
2025-04-16 09:54:00 -0700

    Added more arrays to test
    printed results of multiple tests

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿
 
-int[] anyArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int[] firstArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int[] secondArray = {2, 4, 6, 8, 10, 12, 14};
+int[] thirdArray = {10, 20, 30, 40, 50};
 
-int max = FindMaxInArray(anyArray);
+int max = FindMaxInArray(firstArray);
 
-Console.WriteLine($"The maximum value is: {max}");
 
 int FindMaxInArray(int[] arrayToTest)
 {
@@ -19,3 +20,14 @@ int FindMaxInArray(int[] arrayToTest)
     }
     return currentMax; // return this to assign to the 'max' variable above
 }
+
+void TestAllArrays()
+{
+    Console.WriteLine($"""
+                       Testing all arrays...
+                       First array: {FindMaxInArray(firstArray)}
+                       Second array: {FindMaxInArray(secondArray)}
+                       Third array: {FindMaxInArray(thirdArray)}
+                       """);
+}
+TestAllArrays();
