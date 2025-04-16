9d9dd62b2c3fa93b8d562d666be398045d3856e4
itodobien <daniel.w.garcia@outlook.com>
2025-04-16 09:43:38 -0700

    Initial attempt
    Just iterated over an array and compared the current max value with each value in the array. if the value is greater, then replace with the higher value.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+int[] anyArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+int max = FindMaxInArray(anyArray);
+
+Console.WriteLine($"The maximum value is: {max}");
+
+int FindMaxInArray(int[] arrayToTest)
+{
+    int currentMax = arrayToTest[0];
+    
+    for (int i = 1; i < arrayToTest.Length; i++)
+    {
+        if (arrayToTest[i] > currentMax) // test to see if the current value is greater than the current max
+        {
+            currentMax = arrayToTest[i]; // update the current max if the value is greater than the current max
+        }
+    }
+    return currentMax; // return this to assign to the 'max' variable above
+}
